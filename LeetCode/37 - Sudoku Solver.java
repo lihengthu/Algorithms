3f8a8f50ab2df16a9dee796cd664e00f95803506
DESKTOP-L1VUS65\51wenyu <liheng301@126.com>
2017-08-08 21:49:29 +0800

    2017-08-08 leetcode 37

---
+++ LeetCode/37 - Sudoku Solver.java
@@ -0,0 +1,78 @@
+Analysis: 
+    1. 基本的解法是回溯，还有更为高级的解法;
+
+Solutions:
+
+1. BackTracing
+// Straight forward but slow
+public class Solution {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) return;
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; ++c) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board))
+                                return true;
+                            else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; ++i) {
+            if (board[i][col] != '.' && board[i][col] == c) return false;
+            if (board[row][i] != '.' && board[row][i] == c) return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+// a little improve
+public class Solution {
+    public void solveSudoku(char[][] board) {
+        doSolve(board, 0, 0);
+    }
+    
+    private boolean doSolve(char[][] board, int row, int col) {
+        for (int i = row; i < 9; i++, col = 0) { // note: must reset col here!
+            for (int j = col; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (doSolve(board, i, j + 1))
+                            return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num || 
+                    board[blkrow + i / 3][blkcol + i % 3] == num)
+                return false;
+        return true;
+    }
+}
