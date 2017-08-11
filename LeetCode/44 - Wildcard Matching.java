521a959db3e660a57d97da060200811a3f3d07c7
51wenyu <lihengthu@gmail.com>
2017-08-11 12:21:02 +0800

    2017- 08-11  leetcode 44 通配符匹配，待进一步理解

---
+++ LeetCode/44 - Wildcard Matching.java
@@ -0,0 +1,68 @@
+Analysis: 
+    1. 时间限制，简单的backtracking会超时；
+
+Solutions:
+
+1. 贪心 -- 也是最快的
+http://yucoding.blogspot.sg/2013/02/leetcode-question-123-wildcard-matching.html
+public class Solution {
+    boolean isMatch(String str, String pattern) {
+        int s = 0, p = 0, match = 0, starIdx = -1;            
+        while (s < str.length()){
+            // advancing both pointers
+            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+                s++;
+                p++;
+            }
+            // * found, only advancing pattern pointer
+            else if (p < pattern.length() && pattern.charAt(p) == '*'){
+                starIdx = p;
+                match = s;
+                p++;
+            }
+           // last pattern pointer was *, advancing string pointer
+            else if (starIdx != -1){
+                p = starIdx + 1;
+                match++;
+                s = match;
+            }
+           //current pattern pointer is not star, last patter pointer was not *
+          //characters do not match
+            else return false;
+        }
+        
+        //check for remaining characters in pattern
+        while (p < pattern.length() && pattern.charAt(p) == '*')
+            p++;
+        
+        return p == pattern.length();
+    }
+}
+2. DP -- 
+https://discuss.leetcode.com/topic/22516/my-java-dp-solution-using-2d-table
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] match=new boolean[s.length()+1][p.length()+1];
+        match[s.length()][p.length()]=true;
+        for(int i=p.length()-1;i>=0;i--){
+            if(p.charAt(i)!='*')
+                break;
+            else
+                match[s.length()][i]=true;
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
+                        match[i][j]=match[i+1][j+1];
+                else if(p.charAt(j)=='*')
+                        match[i][j]=match[i+1][j]||match[i][j+1];
+                else
+                    match[i][j]=false;
+            }
+        }
+        return match[0][0];
+    }
+}
+
+C++版本
+https://discuss.leetcode.com/topic/17901/accepted-c-dp-solution-with-a-trick
