f0cee2fcd88a70a810ec99560f35f3b9f0e6f71c
51wenyu <lihengthu@gmail.com>
2017-08-17 20:21:30 +0800

    2017-08-17 leetcode 54 剑指 offer 14 & 20

---
+++ Coding Interviews--Questions Analysis and Solutions/14 - ReorderArray.java
@@ -0,0 +1,47 @@
+Analysis:
+
+
+Solutions:
+1. // 自己写的 -- 时间复杂度：O(n), 空间复杂度：O(n)
+public class Solution {
+    public void reOrderArray(int [] array) {
+        if (array == null || array.length == 0) return;
+        int[] copy = new int[array.length];
+        int index = 0;
+        for (int i = 0; i < array.length; ++i){
+            if (array[i] % 2 == 1)
+                copy[index++] = array[i];
+        }
+        for (int i = 0; i < array.length; ++i){
+            if (array[i] % 2 == 0)
+                copy[index++] = array[i];
+        }
+        for (int i = 0; i < array.length; ++i)
+            array[i] = copy[i];
+    }
+}
+
+2. // Discuss (1) isEven 考虑可扩展性；（2）容易忽略的点，if-else block, 能写出bug-free代码需要功底；
+public class Solution {
+    public void reOrderArray(int[] array) {
+        if (array == null || array.length == 0) return;
+        int i = 0, j;
+        while (i < array.length) {
+            while (i < array.length && !isEven(array[i]))
+                ++i;
+            j = i + 1;
+            while (j < array.length && isEven(array[j]))
+                ++j;
+            if (j < array.length) {
+                int tmp = array[j];
+                for (int k = j; j > i; --j)
+                    array[j] = array[j - 1];
+                array[i++] = tmp;
+            } else break;
+        }
+    }
+
+    private boolean isEven(int n) {
+        return (n % 2 == 0) ? true : false;
+    }
+}
