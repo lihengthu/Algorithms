40c463a3f89a20cb25e6718f086696059387a396
51wenyu <lihengthu@gmail.com>
2017-08-31 23:37:20 +0800

    LeetCode 94 & 105

---
+++ LeetCode/94 - Binary Tree Inorder Traversal.java
@@ -0,0 +1,39 @@
+Analysis: 
+
+Solutions:
+
+1. // 递归 -- 力争 clean and concise
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            helper(root.left, list);
+            list.add(root.val);
+            helper(root.right, list);
+        }
+    }
+}
+
+2. // Iterative, 反而没有递归快？？
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
