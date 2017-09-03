847de1a424f4507ff589f51d1bb2457946324efe
DESKTOP-L1VUS65\51wenyu <liheng301@126.com>
2017-09-03 23:21:58 +0800

    LeetCode 74 & 240

---
+++ LeetCode/74 - Search a 2D Matrix.java
@@ -0,0 +1,42 @@
+Analysis: 
+    1. Fabinacci
+
+Solutions:
+
+1. 从右上角二分, basic idea
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            if (matrix[row][col] < target)
+                ++row;
+            else --col;
+        }
+        return false;
+    }
+}
+2. //Don't treat it as a 2D matrix, just treat it as a sorted list
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int start = 0, rows = matrix.length, cols = matrix[0].length;
+        int end = rows * cols - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (matrix[mid / cols][mid % cols] == target) {
+                return true;
+            }
+            if (matrix[mid / cols][mid % cols] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
