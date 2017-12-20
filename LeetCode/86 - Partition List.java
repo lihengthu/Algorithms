6acbf88429399573edff4f9c2b52631f245cacc5
51wenyu <lihengthu@gmail.com>
2017-12-20 23:54:00 +0800

    LeetCode 86

---
+++ LeetCode/86 - Partition List.java
@@ -0,0 +1,29 @@
+Analysis: 
+	1. 
+
+Solutions:
+
+1. 
+class Solution {
+	public ListNode partition(ListNode head, int x) {
+		if (head == null){
+			return head;
+		}
+		ListNode leftDummy = new ListNode(0);
+		ListNode rightDummy = new ListNode(0);
+		ListNode left = leftDummy, right = rightDummy;
+		while(head != null){
+			if (head.val < x){
+				left.next = head;
+				left = head;
+			} else {
+				right.next = head;
+				right = head;
+			}
+			head = head.next;
+		}
+		right.next = null;
+		left.next = rightDummy.next;
+		return leftDummy.next;
+	}
+}
