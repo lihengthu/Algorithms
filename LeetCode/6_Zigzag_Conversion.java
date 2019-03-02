48f5e5c44b50ff7351a40f17f15fe7a3970d5462
WenYu <lihengthu@gmail.com>
2019-03-02 10:11:04 +0800

    LeetCode 6

--- LeetCode/6_Zigzag_Conversion.java
+++ LeetCode/6_Zigzag_Conversion.java
@@ -1,6 +1,25 @@
-Personal:
+Best Solution:
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        int cycle = 2 * numRows - 2;
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j + i < n; j += cycle) {
+                sb.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycle - i < n) {
+                    sb.append(s.charAt(j + cycle - i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
 
-1st version:
+Personal version:
 class Solution {
     public String convert(String s, int numRows) {
         if (s.length() == 0 || numRows <= 1) {
