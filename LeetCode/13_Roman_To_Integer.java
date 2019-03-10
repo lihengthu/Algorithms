65d3282881ee59c2882aac6d4ca3406cb03bd2ef
WenYu <lihengthu@gmail.com>
2019-03-10 21:29:03 +0800

    LeetCode 13

---
+++ LeetCode/13_Roman_To_Integer.java
@@ -0,0 +1,70 @@
+
+Solutions:
+1. 
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+
+        int sum = 0;
+        int prev = map.get(s.charAt(0));
+        int next = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            next = map.get(s.charAt(i));
+            if (prev < next) {
+                sum -= prev;
+            } else {
+                sum += prev;
+            }
+            prev = next;
+        }
+        sum += prev;//corner case when only one digit, we need to let sum = prev, so we add prev, not next
+        return sum;
+    }
+}
+
+
+2. Nice!!
+class Solution {
+    public static int romanToInt(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'I':
+                    res += (res >= 5 ? -1 : 1);
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    res += 10 * (res >= 50 ? -1 : 1);
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    res += 100 * (res >= 500 ? -1 : 1);
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+            }
+        }
+        return res;
+    }
+}
+
