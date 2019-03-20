e7c9e2cc07d219e3a1241930678c697736008d84
WenYu <lihengthu@gmail.com>
2019-03-20 08:42:48 +0800

    LeetCode 15

---
+++ LeetCode/15_3Sum.java
@@ -0,0 +1,28 @@
+
+Solutions: 
+1.
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) ++lo;
+                        while (hi > lo && nums[hi] == nums[hi - 1]) --hi;
+                        ++lo;
+                        --hi;
+                    } else if (nums[lo] + nums[hi] < sum) {
+                        ++lo;
+                    } else {
+                        --hi;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
