6fd47a3e41d7bcee7971a6bb8e78c59733070708
WenYu <lihengthu@gmail.com>
2019-07-01 23:04:51 +0800

    Create 20_Valid_Parentheses.java

---
+++ LeetCode/20_Valid_Parentheses.java
@@ -0,0 +1,54 @@
+
+Solutions: 
+
+1.
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if ("([{".contains(String.valueOf(c))) {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty() && is_valid(stack.peek(), c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean is_valid(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']')
+                || (c1 == '{' && c2 == '}');
+    }
+}
+
+2. 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
