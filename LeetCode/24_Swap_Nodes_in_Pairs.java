d3fcb08b83650a46ad5ebf47b8ba4295e94c1a03
WenYu <lihengthu@gmail.com>
2019-07-05 09:25:30 +0800

    Create 24_Swap_Nodes_in_Pairs.java

---
+++ LeetCode/24_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,20 @@
+Analysis: 
+    1. 
+
+Solutions:
+1.
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        while (head.next != null && head.next.next != null) {
+            ListNode n1 = head.next, n2 = head.next.next;
+            head.next = n2;
+            n1.next = n2.next;
+            n2.next = n1;
+            head = n1;
+        }
+        return dummy.next;
+    }
+}
