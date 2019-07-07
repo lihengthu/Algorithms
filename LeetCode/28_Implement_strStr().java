8d5c014b781701c7daefde53793714b192553637
WenYu <lihengthu@gmail.com>
2019-07-07 23:02:46 +0800

    Create 28_Implement_strStr().java

---
+++ LeetCode/28_Implement_strStr().java
@@ -0,0 +1,20 @@
+// 1.
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle == null || needle.isEmpty()) {
+            return 0;
+        }
+        for (int i = 0; i < haystack.length() - needle.length() + 1; ++i) {
+            int j = 0;
+            for (j = 0; j < needle.length(); ++j) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
