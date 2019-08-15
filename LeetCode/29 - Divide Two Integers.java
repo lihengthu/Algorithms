6e35224f5bb3ecf97147457b32c2a857bf6e84b8
WenYu <lihengthu@gmail.com>
2019-08-15 09:34:38 +0800

    Update 29 - Divide Two Integers.java

--- LeetCode/29 - Divide Two Integers.java
+++ LeetCode/29 - Divide Two Integers.java
@@ -1,27 +1,26 @@
-Analysis: 
-    1. 位运算 Bit Manipulation
-    2. 分析出 溢出的两种情况：(1) divisor == 0; (2) dividend = INT_MIN and divisor = -1 (because abs(INT_MIN) = INT_MAX + 1).
-    3. 把dividend和divisor 转成long, 再取求abs; 因为 Math.abs(Integer.MIN_VALUE) == Integer.MIN_VALUE;
-    参考：https://stackoverflow.com/questions/5444611/math-abs-returns-wrong-value-for-integer-min-value
-Solutions:
-
-1. 位运算
-public class Solution {
+class Solution {
     public int divide(int dividend, int divisor) {
-        if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1))
-            return Integer.MAX_VALUE;
-        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-        long dvd = Math.abs((long)dividend), dvs = Math.abs((long)divisor);
-        int res = 0;
-        while (dvd >= dvs) {
-            long temp = dvs, multiple = 1;
-            while (dvd >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-            dvd -= temp;
-            res += multiple;
+        if (divisor == 0) {
+            return dividend > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
         }
-        return sign == 1 ? res : -res;
+        if (dividend == 0) {
+            return 0;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean isNeg = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        int result = 0;
+        while (a >= b) {
+            int shift = 0;
+            while (a >= (b << shift)) {
+                ++shift;
+            }
+            a -= b << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+        return isNeg ? -result : result;
     }
 }
