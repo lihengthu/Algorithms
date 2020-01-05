7d438c8e4a825b839deef4a82e101ea907749a44
WenYu <lihengthu@gmail.com>
2020-01-05 22:03:00 +0800

    LeetCode 45

---
+++ LeetCode/45_Jump_Game_II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0, end = 0, jumps = 0;
+        while (end < nums.length - 1) {
+            ++jumps;
+            int farthest = end;
+            for (int i = start; i <= end; ++i) {
+                if (nums[i] + i > farthest) {
+                    farthest = nums[i] + i;
+                }
+            }
+            start = end + 1;
+            end = farthest;
+        }
+        return jumps;
+    }
+
+}
