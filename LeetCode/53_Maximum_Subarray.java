7c6b45b44cd0761ca94f93fb04d40746e88e369b
WenYu <lihengthu@gmail.com>
2020-03-10 22:00:04 +0800

    LC 53

---
+++ LeetCode/53_Maximum_Subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            sum = Math.max(sum, 0);
+        }
+        return max;
+    }
+}
