e664b8ae816a232484a92ba713e6d4710261d4a5
Li Heng <liheng@cn.ibm.com>
2020-04-05 08:47:21 +0800

    LC 62

---
+++ LeetCode/62_Unique_Paths.java
@@ -0,0 +1,34 @@
+1. Formula
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int k = Math.min(m - 1, n - 1);
+        long result = 1;
+        for (int i = 1; i <= k; ++i) {
+            result = result * (N - i + 1) / i;
+        }
+        return (int)result;
+    }
+}
+
+2. DP
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0){
+            return 1;
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; ++i) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
