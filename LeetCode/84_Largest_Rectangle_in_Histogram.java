59706a680ffa8dc8af1c8a43097b78d9b9853fd1
Li Heng <liheng@cn.ibm.com>
2020-06-03 09:48:16 +0800

    LC 84

--- LeetCode/84_Largest_Rectangle_in_Histogram.java
+++ LeetCode/84_Largest_Rectangle_in_Histogram.java
@@ -1,22 +1,16 @@
-Analysis: 
-	1. 单调栈
-
-Solutions:
-
-1. 
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        if (heights == null || heights.length == 0){
+        if (heights == null || heights.length == 0) {
             return 0;
         }
         int max = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i <= heights.length; ++i){
+        for (int i = 0; i <= heights.length; i++) {
             int cur = (i == heights.length) ? -1 : heights[i];
-            while (!stack.isEmpty() && cur <= heights[stack.peek()]){
+            while (!stack.isEmpty() && cur <= heights[stack.peek()]) {
                 int h = heights[stack.pop()];
-                int w = stack.isEmpty() ? i : (i - stack.peek() - 1);
-                max = Math.max(max,h * w);
+                int w = stack.isEmpty() ? 1 : i - stack.peek() - 1;
+                max = Math.max(max, h * w);
             }
             stack.push(i);
         }
