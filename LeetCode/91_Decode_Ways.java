e48f348f577d38c9107ab573b10635e2a1c5a810
Li Heng <liheng@cn.ibm.com>
2020-06-21 00:29:49 +0800

    LeetCode 91

---
+++ LeetCode/91_Decode_Ways.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+            int val = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+            if (val >= 10 && val <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
