84bf87f5a46be582a569658e13597ceee9dfff27
Li Heng <lihengthu@gmail.com>
2020-06-22 21:05:33 +0800

    LC 92 & 206

--- LeetCode/206_Reverse_Linked_List.java
+++ LeetCode/206_Reverse_Linked_List.java
@@ -10,19 +10,3 @@ class Solution {
         return prev;
     }
 }
-
-2. 递归
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        return reverseListInt(head, null);
-    }
-
-    private ListNode reverseListInt(ListNode head, ListNode newHead) {
-        if (head == null) {
-            return newHead;
-        }
-        ListNode next = head.next;
-        head.next = newHead;
-        return reverseListInt(next, head);
-    }
-}
