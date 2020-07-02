79ed6a4da84c8456b525a0970ecbd1131a73175d
Li Heng <liheng@cn.ibm.com>
2020-07-02 20:56:03 +0800

    LeetCode 97 & 98

--- LeetCode/98_Validate_Binary_Search_Tree.java
+++ LeetCode/98_Validate_Binary_Search_Tree.java
@@ -1,55 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-1. Recursive Solution
-class Solution {
-    private int lastVal = Integer.MIN_VALUE;
-    private boolean firstNode = true;
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        if (!isValidBST(root.left)) {
-            return false;
-        }
-        if (!firstNode && lastVal >= root.val) {
-            return false;
-        }
-        firstNode = false;
-        lastVal = root.val;
-        if (!isValidBST(root.right)) {
-            return false;
-        }
-        return true;
-    }
-}
-
-2. Iterative Solution
+// 1. Iterative
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        TreeNode pre = null;
         Stack<TreeNode> stack = new Stack<>();
+        long prev = Long.MIN_VALUE;
+
         while (root != null || !stack.isEmpty()) {
             while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
             root = stack.pop();
-            if (pre != null && root.val <= pre.val){
+            if (root.val <= prev) {
                 return false;
             }
-            pre = root;
+            prev = root.val;
             root = root.right;
         }
         return true;
+    }
+}
+
+// 2. Recursive
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean helper(TreeNode root, long lower, long upper) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= lower || root.val >= upper) {
+            return false;
+        }
+
+        return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
     }
 }
