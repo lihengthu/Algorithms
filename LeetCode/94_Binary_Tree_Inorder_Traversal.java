b2afa0f8cbbdc3e6b7f8ee991a75b06f059f4e9c
Li Heng <lihengthu@gmail.com>
2020-07-03 08:20:04 +0800

    LeetCode 94 & 96

---
+++ LeetCode/94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+
+        return result;
+    }
+}
--- LeetCode/96_Unique_Binary_Search_Trees.java
+++ LeetCode/96_Unique_Binary_Search_Trees.java
@@ -1,13 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-		int[] count = new int[n + 1];
-		count[0] = 1;
-		count[1] = 1;
-		for (int i = 2; i <= n; ++i){
-			for (int j = 0; j < i; ++j){
-				count[i] += count[j] * count[i - j - 1];
-			}
-		}
-		return count[n];
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+
+        return dp[n];
     }
 }
