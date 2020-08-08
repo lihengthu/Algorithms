f019da5ccef5930f6c76fbb4e84b3d650060e875
Li Heng <liheng@cn.ibm.com>
2020-08-08 18:33:09 +0800

    LC 166 & 172

---
+++ LeetCode/172_Factorial_Trailing_Zeroes.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        if (n >= 5) {
+            return n / 5 + trailingZeroes(n / 5);
+        } else {
+            return 0;
+        }
+    }
+}
