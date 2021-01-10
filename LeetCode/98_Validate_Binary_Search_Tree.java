b9234c5a780eb2ce3874c7e5aeda05132b1bc4ee
Li Heng <liheng@cn.ibm.com>
2021-01-10 12:16:17 +0800

    Update 98_Validate_Binary_Search_Tree.java

--- LeetCode/98_Validate_Binary_Search_Tree.java
+++ LeetCode/98_Validate_Binary_Search_Tree.java
@@ -2,39 +2,42 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
-        long prev = Long.MIN_VALUE;
-
+        Integer prev = null;
         while (root != null || !stack.isEmpty()) {
             while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
             root = stack.pop();
-            if (root.val <= prev) {
+            if (prev != null && root.val <= prev) {
                 return false;
             }
-            prev = root.val;
             root = root.right;
         }
+
         return true;
     }
 }
 
 // 2. Recursive
 class Solution {
+    private Integer prev = null;
+
     public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return inorder(root);
     }
 
-    private boolean helper(TreeNode root, long lower, long upper) {
+    private boolean inorder(TreeNode root) {
         if (root == null) {
             return true;
         }
-
-        if (root.val <= lower || root.val >= upper) {
+        if (!inorder(root.left)) {
             return false;
         }
-
-        return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        this.prev = root.val;
+        return inorder(root.right);
     }
 }
