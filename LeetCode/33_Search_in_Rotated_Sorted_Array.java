1d7ee8ba74f1f8dca32dc913585fcd031db3a51c
liheng <lihengthu@gmail.com>
2024-06-10 23:01:26 +0800

    Update

--- LeetCode/33_Search_in_Rotated_Sorted_Array.java
+++ LeetCode/33_Search_in_Rotated_Sorted_Array.java
@@ -1,31 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-
-        int n = nums.length;
         int l = 0, r = nums.length - 1;
         while (l <= r) {
-            int mid = (l + r) >>> 1;
+            int mid = l + (r - l) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] <= nums[r]) {
-                if (target <= nums[r] && nums[mid] < target) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else {
+            
+            if (nums[mid] >= nums[l]) {
                 if (nums[l] <= target && target < nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
+            } else if (nums[mid] < nums[l]) {
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
-
+        
         return -1;
     }
 }
--- LeetCode/81_Search_in_Rotated_Sorted_Array_II.java
+++ LeetCode/81_Search_in_Rotated_Sorted_Array_II.java
@@ -1,33 +1,29 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return false;
-        }
-
-        int n = nums.length;
-        int l = 0, r = n - 1;
+        int l = 0, r = nums.length - 1;
         while (l <= r) {
-            int mid = (l + r) >>> 1;
+            int mid = l + (r - l) / 2;
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] < nums[r]) {
-                if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else if (nums[mid] > nums[r]) {
+
+            if (nums[mid] == nums[l]) {
+                l++;
+            } else if (nums[mid] > nums[l]) {
                 if (nums[l] <= target && target < nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
-            } else {
-                r--;
+            } else if (nums[mid] < nums[l]) {
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
-        
+
         return false;
     }
 }
