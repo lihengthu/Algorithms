126f816a59d6f4a1da0c581eaf04022a4051b5dc
liheng <lihengthu@gmail.com>
2024-10-13 21:03:16 +0800

    LC 4

--- LeetCode/4_Median_of_Two_Sorted_Arrays.java
+++ LeetCode/4_Median_of_Two_Sorted_Arrays.java
@@ -1,87 +1,42 @@
-// 1. https://leetcode.com/problems/median-of-two-sorted-arrays/solution/
+// 直接查找的O(n)的方法也要会写
+// 详细解释：https://leetcode.cn/problems/median-of-two-sorted-arrays/solutions/258842/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double result = 0;
-        if (nums1.length > nums2.length) {
-            int[] temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
-        }
-
         int m = nums1.length, n = nums2.length;
-        int half = (m + n + 1) >>> 1;
-        int l = 0, r = m;
-        while (l <= r) {
-            int i = (l + r) >>> 1;
-            int j = half - i;
-            if (i < m && nums2[j - 1] > nums1[i]) {
-                l = i + 1;
-            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
-                r = i - 1;
-            } else {
-                double lmax = 0, rmin = 0;
-                if (i == 0) {
-                    lmax = nums2[j - 1];
-                } else if (j == 0) {
-                    lmax = nums1[i - 1];
-                } else {
-                    lmax = Math.max(nums1[i - 1], nums2[j - 1]);
-                }
+        int total = m + n;
 
-                // m + n 为奇数的情况
-                if (((m + n) & 1) == 1) {
-                    return lmax;
-                }
-
-                if (i == m) {
-                    rmin = nums2[j];
-                } else if (j == n) {
-                    rmin = nums1[i];
-                } else {
-                    rmin = Math.min(nums1[i], nums2[j]);
-                }
-                result = (lmax + rmin) / 2;
-                break;
-            }
+        if (total % 2 == 1) {
+            return getKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2 + 1);
         }
 
-        return result;
-    }
-}
-
-// 2. https://www.jiuzhang.com/solution/median-of-two-sorted-arrays/#tag-lang-ALL
-class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length + nums2.length;
-        if ((n & 1) == 0) {
-            return (findKth(nums1, nums2, 0, 0, n / 2) +
-                    findKth(nums1, nums2, 0, 0, n / 2 + 1)) / 2.0;
-        }
-
-        return findKth(nums1, nums2, 0, 0, n / 2 + 1);
+        int firstValue = getKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2);
+        int secondValue = getKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2 + 1);
+        return (firstValue + secondValue) / 2.0;
     }
 
-    // 找两个有序数组的第k大数
-    // Divide and Conquer
-    public int findKth(int[] A, int[] B, int startA, int startB, int k) {
-        if (startA >= A.length) {
-            return B[startB + k - 1];
+    // 找第k小的元素
+    public int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        if (start1 > end1) {
+            return nums2[start2 + k - 1];
+        } else if (start2 > end2) {
+            return nums1[start1 + k - 1];
         }
-        if (startB >= B.length) {
-            return A[startA + k - 1];
-        }
+
         if (k == 1) {
-            return Math.min(A[startA], B[startB]);
-        }
-
-        int halfKthA = startA + k / 2 - 1 < A.length ? A[startA + k / 2 - 1] : Integer.MAX_VALUE;
-        int halfKthB = startB + k / 2 - 1 < B.length ? B[startB + k / 2 - 1] : Integer.MAX_VALUE;
-
-        // 注意k - k/2
-        if (halfKthA < halfKthB) {
-            return findKth(A, B, startA + k / 2, startB, k - k / 2);
+            return Math.min(nums1[start1], nums2[start2]);
         } else {
-            return findKth(A, B, startA, startB + k / 2, k - k / 2);
+            int d = k / 2 - 1;
+            int validLeft = Math.min(start1 + d, end1);
+            int validRight = Math.min(start2 + d, end2);
+
+            // 谁小谁排除
+            if (nums1[validLeft] <= nums2[validRight]) {
+                k -= (validLeft - start1 + 1);
+                return getKth(nums1, validLeft + 1, end1, nums2, start2, end2, k);
+            } else {
+                k -= (validRight - start2 + 1);
+                return getKth(nums1, start1, end1, nums2, validRight + 1, end2, k);
+            }
         }
     }
 }
